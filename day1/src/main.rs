27843df7d43dd6badb2377e0a95297eaec256a02
tomas-mota <tomasrebelomota@gmail.com>
2022-12-04 19:50:49 +0100

    day1 part2
    Signed-off-by: tomas-mota <tomasrebelomota@gmail.com>

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,18 +1,33 @@
 use std::fs;
 
 fn main() {
-    part1() 
+    part1();
+    part2();
 }
 
 fn part1() {
     let contents = fs::read_to_string("input.txt").expect("Should have read the file");
 
-    let max = contents
+    let max: Option<u32> = contents
         .split("\n\n")
-        .map(|x| {
-            x.split("\n").flat_map(|x| x.parse::<u32>()).sum::<u32>()
-        })
+        .map(|x| x.split("\n").flat_map(|x| x.parse::<u32>()).sum())
         .max();
 
-    println!("{:?}", max);
+    println!("{}", max.unwrap());
+}
+
+fn part2() {
+    let contents = fs::read_to_string("input.txt").expect("Should have read the file");
+
+    let mut counts: Vec<u32> = contents
+        .split("\n\n")
+        .map(|x| x.split("\n").flat_map(|x| x.parse::<u32>()).sum())
+        .collect();
+
+    counts.sort();
+
+    println!(
+        "{}",
+        counts[counts.len() - 1] + counts[counts.len() - 2] + counts[counts.len() - 3]
+    );
 }
