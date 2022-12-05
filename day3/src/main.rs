1ec957e16f0ef2e7524d537d5a9b98c0272d837d
tomas-mota <tomasrebelomota@gmail.com>
2022-12-05 21:47:56 +0100

    day3 finished
    Signed-off-by: tomas-mota <tomasrebelomota@gmail.com>

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,8 +1,10 @@
+#![feature(iter_array_chunks)]
 const INPUT: &str = include_str!("../input.txt");
 const LETTERS: &str = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 fn main() {
-    part1()
+    part1();
+    part2();
 }
 
 fn part1() {
@@ -13,11 +15,37 @@ fn part1() {
         let left = line.get(..line.len() / 2).unwrap();
         let right = line.get(line.len() / 2..).unwrap();
 
-        sum = sum + LETTERS.find(|x| x == common_char(left, right)).unwrap();
+        sum += LETTERS.find(|x| x == common_char_in2(left, right)).unwrap();
     }
+    println!("{}", sum);
 }
 
-fn common_char(str1: &str, str2: &str) -> char {
+fn part2() {
+    let lines = INPUT.lines();
+
+    let mut sum = 0;
+    for [x, y, z] in lines.into_iter().array_chunks() {
+        sum += LETTERS.find(|s| s == common_char_in3(x, y, z)).unwrap();
+    }
+    println!("{}", sum);
+}
+
+fn common_char_in3(str1: &str, str2: &str, str3: &str) -> char {
+    for l1 in str1.chars() {
+        for l2 in str2.chars() {
+            if l1 == l2 {
+                for l3 in str3.chars() {
+                    if l2 == l3 {
+                        return l1;
+                    }
+                }
+            }
+        }
+    }
+    panic!("unknown char");
+}
+
+fn common_char_in2(str1: &str, str2: &str) -> char {
     for lc in str1.chars() {
         for rc in str2.chars() {
             if lc == rc {
