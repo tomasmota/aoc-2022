93d4d7b8e3842e73c7e677209f0e1ba41b7b61ca
Thomas Mota <tmm@danskecommodities.com>
2022-12-06 15:28:30 +0100

    day4 progress
    Signed-off-by: Thomas Mota <tmm@danskecommodities.com>

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,6 +1,8 @@
 // const INPUT: &str = include_str!("../input.txt");
 
-use std::str::Split;
+// use std::str::Split;
+
+use std::ops::Range;
 
 fn main() {
     part1()
@@ -9,13 +11,32 @@ fn main() {
 fn part1() {
     let input = "71-71,42-72";
 
-    let split: Split<&str> = input.split(",");
-    let mapped = split.into_iter().map(|x| {
-        let y = x.split("-").map(|x| x.parse::<i32>());
+    let pair = input.split(",").collect::<Vec<&str>>();
 
-        println!("{:?}", y);
-        println!("{}", x);
-        (x, x)
-    });
-    println!("{:?}", mapped.collect::<Vec<(&str, &str)>>())
+    let nested = pair
+        .into_iter()
+        .map(|x| x.split("-").collect::<Vec<&str>>());
+
+    // println!("{:?}", nested.collect::<Vec<Vec<&str>>>())
+
+    let ranges: Vec<Range<i32>> = nested
+        .map(|pair| Range::<i32> {
+            start: pair[0].parse::<i32>().unwrap(),
+            end: pair[1].parse::<i32>().unwrap(),
+        })
+        .collect();
+
+    let g1 = &ranges[0];
+    &ranges[1].all(|x| g1.contains(&x));
+
+    println!("{:?}", g1);
+    println!("{:?}", ranges);
+    // let mapped = split.into_iter().map(|x| {
+    //     let y = x.split("-").into_iter().map(|x| x.parse::<i32>());
+    //
+    //     println!("{:?}", y);
+    //     println!("{}", x);
+    //     (x, x)
+    // });
+    // println!("{:?}", mapped.collect::<Vec<(&str, &str)>>())
 }
