20ecbc15540e1e5637e34a0652d6e15746e7f620
Thomas Mota <tmm@danskecommodities.com>
2022-12-07 15:11:06 +0100

    day 4 done
    Signed-off-by: Thomas Mota <tmm@danskecommodities.com>

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,9 +1,10 @@
 const INPUT: &str = include_str!("../input.txt");
 
-use std::ops::Range;
+use std::{cmp, ops::Range};
 
 fn main() {
-    part1()
+    part1();
+    part2();
 }
 
 fn part1() {
@@ -29,6 +30,37 @@ fn part1() {
     });
 
     println!("{}", sum);
+}
+
+fn part2() {
+    let sum = INPUT.lines().fold(0, |accum, x| {
+        let n = x
+            .split(",")
+            .map(|x| {
+                let mut split = x.split("-").map(|x| x.parse::<i32>().unwrap());
+                return Range {
+                    start: split.next().unwrap(),
+                    end: split.next().unwrap(),
+                };
+            })
+            .collect::<Vec<Range<i32>>>();
+
+        let left = n.get(0).unwrap();
+        let right = n.get(1).unwrap();
+        if overlapping(left, right) {
+            return accum + 1;
+        }
+
+        return accum;
+    });
+
+    println!("{}", sum);
+}
+
+fn overlapping(left: &Range<i32>, right: &Range<i32>) -> bool {
+    let total_range = cmp::max(left.end, right.end) - cmp::min(left.start, right.start);
+    let summed_ranges = (left.end - left.start) + (right.end - right.start);
+    return summed_ranges >= total_range;
 }
 
 fn contained(left: &Range<i32>, right: &Range<i32>) -> bool {
