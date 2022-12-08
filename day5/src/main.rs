7fadea686b6c505568ce8ddf399922a9a89cb8cf
Thomas Mota <tmm@danskecommodities.com>
2022-12-08 08:08:28 +0100

    day5 setup
    Signed-off-by: Thomas Mota <tmm@danskecommodities.com>

---
+++ day5/src/main.rs
@@ -0,0 +1,13 @@
+// const INPUT: &str = include_str!("../input.txt");
+
+fn main() {
+    part1();
+}
+
+fn part1() {
+    // Create 9 stacks
+    // Starting in line with index 7, parse crates and push them into respective stack. Split on spaces
+    // Do this until the first line is reached
+    // Starting on line with index 10, pop the given number of crates, popping and pushing them one at
+    // a time.
+}
