f672ac272aa580280129b37565d3f44a8beb1b2d
tomas-mota <tomasrebelomota@gmail.com>
2022-12-11 19:22:58 +0100

    finish day6
    Signed-off-by: tomas-mota <tomasrebelomota@gmail.com>

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,11 +1,11 @@
-#![feature(generic_arg_infer)]
-#![feature(iter_array_chunks)]
 use itertools::Itertools;
+use std::collections::VecDeque;
 
-const INPUT: &str = include_str!("../input.test.txt");
+const INPUT: &str = include_str!("../input.txt");
 
 fn main() {
-    part1()
+    part1();
+    part2();
 }
 
 fn part1() {
@@ -20,4 +20,29 @@ fn part1() {
     }
 
     println!("{}", index + 4);
+}
+
+fn part2() {
+    let chars = INPUT
+        .lines()
+        .nth(0)
+        .expect("first line is present")
+        .char_indices();
+
+    let mut window: VecDeque<char> = VecDeque::new();
+
+    for (i, c) in chars {
+        // for first 14 chars, just put them in the window
+        if i < 14 {
+            window.push_back(c);
+        } else {
+            // after filling the window, move through until all chars are unique
+            if window.clone().into_iter().all_unique() {
+                println!("{}", i);
+                return;
+            }
+            window.pop_front();
+            window.push_back(c);
+        }
+    }
 }
